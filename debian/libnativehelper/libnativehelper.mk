b14c7db956b959d7fd77e8cc7c47c47fabd14060
Prateek Ganguli <prateek.ganguli@gmail.com>
2021-04-16 01:32:38 +0530

    Adapt paths for libnativehelper

--- debian/libnativehelper/libnativehelper.mk
+++ debian/libnativehelper/libnativehelper.mk
@@ -2,20 +2,21 @@ NAME = libnativehelper
 SOURCES = JNIHelp.cpp \
           JniConstants.cpp \
           toStringArray.cpp \
-          JniInvocation.cpp \
+          JniInvocation.cpp
+SOURCES := $(foreach source, $(SOURCES), libnativehelper/$(source))
 
 CXXFLAGS += -std=c++17
 CPPFLAGS += \
-  -Iinclude_jni \
-  -Iinclude \
-  -Iheader_only_include \
-  -Iplatform_include \
+  -Ilibnativehelper/include_jni \
+  -Ilibnativehelper/include \
+  -Ilibnativehelper/header_only_include \
+  -Ilibnativehelper/platform_include \
   -I/usr/include/android \
 
 LDFLAGS += -shared -Wl,-soname,$(NAME).so.0 -ldl -lpthread \
            -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
            -L/usr/lib/$(DEB_HOST_MULTIARCH)/android -llog
 
-build: $(SOURCES)
-	$(CXX) $^ -o $(NAME).so.0 $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
-	ln -s $(NAME).so.0 $(NAME).so
+libnativehelper/$(NAME).so.0: $(SOURCES)
+	$(CXX) $^ -o libnativehelper/$(NAME).so.0 $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
+	ln -s $(NAME).so.0 libnativehelper/$(NAME).so
