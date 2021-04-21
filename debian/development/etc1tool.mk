f5b542b7b5f31348755088dea27b09a241c26d45
Prateek Ganguli <prateek.ganguli@gmail.com>
2021-04-21 11:17:05 +0530

    Adapt paths for development

--- debian/development/etc1tool.mk
+++ debian/development/etc1tool.mk
@@ -1,12 +1,12 @@
 NAME = etc1tool
 SOURCES = etc1tool.cpp
-SOURCES := $(foreach source, $(SOURCES), tools/etc1tool/$(source))
-CPPFLAGS += -Iinclude
+SOURCES := $(foreach source, $(SOURCES), development/tools/etc1tool/$(source))
+CPPFLAGS += -Idevelopment/include -I/usr/include/android
 LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
            -lpng -L/usr/lib/$(DEB_HOST_MULTIARCH)/android -lETC1
 
-$(NAME): $(SOURCES)
+development/$(NAME): $(SOURCES)
 	$(CXX) $^ -o $@ $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
 
 clean:
-	$(RM) $(NAME)
+	$(RM) development/$(NAME)
