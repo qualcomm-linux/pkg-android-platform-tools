e703f66064fcdcb49d811e7e4537f2211871949c
Prateek Ganguli <prateek.ganguli@gmail.com>
2021-04-23 14:31:32 +0530

    Adapt paths for frameworks/native

--- debian/frameworks/native/libETC1.mk
+++ debian/frameworks/native/libETC1.mk
@@ -1,11 +1,10 @@
 NAME = libETC1
 SOURCES = etc1.cpp
-SOURCES := $(foreach source, $(SOURCES), opengl/libs/ETC1/$(source))
-CPPFLAGS += -Iopengl/include
+SOURCES := $(foreach source, $(SOURCES), frameworks/native/opengl/libs/ETC1/$(source))
+CPPFLAGS += -Iframeworks/native/opengl/include
 LDFLAGS += -shared -Wl,-soname,$(NAME).so.0
 
-debian/out/$(NAME).so.0: $(SOURCES)
-	mkdir --parents debian/out
+debian/out/frameworks/native/$(NAME).so.0: $(SOURCES)
+	mkdir --parents debian/out/frameworks/native
 	$(CXX) $^ -o $@ $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
-	ln -s $(NAME).so.0 debian/out/$(NAME).so
-	dpkg-gensymbols -pandroid-libetc1 -v0 -e$@ -Odebian/android-libetc1.symbols
+	ln -s $(NAME).so.0 debian/out/frameworks/native/$(NAME).so
