6b85c3a2bc8e89d5430abde668991610d62f9bc4
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-26 15:07:57 +0530

    Revert "Import files for build"
    This reverts commit 8bf5fb732d519eab45a50f6f87d395c6fd2cc04e.

--- debian/build/make/makeparallel.mk
+++
@@ -1,9 +0,0 @@
-NAME = makeparallel
-SOURCES = makeparallel.cpp
-SOURCES := $(foreach source, $(SOURCES), tools/makeparallel/$(source))
-
-build: $(SOURCES)
-	$(CXX) $^ -o $(NAME) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
-
-clean:
-	$(RM) $(NAME)
--- debian/build/make/ziptime.mk
+++
@@ -1,9 +0,0 @@
-NAME = ziptime
-SOURCES = ZipTime.cpp ZipEntry.cpp ZipFile.cpp
-SOURCES := $(foreach source, $(SOURCES), tools/ziptime/$(source))
-
-build: $(SOURCES)
-	$(CXX) $^ -o $(NAME) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
-
-clean:
-	$(RM) $(NAME)
