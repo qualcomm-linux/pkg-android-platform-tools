39aa1599b189976c7d78bd0fbb932a917349b018
Prateek Ganguli <prateek.ganguli@gmail.com>
2021-04-30 18:46:02 +0530

    Remove dependency android-libetc1-dev

--- debian/development/etc1tool.mk
+++ debian/development/etc1tool.mk
@@ -1,9 +1,10 @@
 NAME = etc1tool
 SOURCES = etc1tool.cpp
 SOURCES := $(foreach source, $(SOURCES), development/tools/etc1tool/$(source))
-CPPFLAGS += -Idevelopment/include -I/usr/include/android
+CPPFLAGS += -Idevelopment/include -I/usr/include/android -Iframeworks/native/opengl/include
 LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
-           -lpng -L/usr/lib/$(DEB_HOST_MULTIARCH)/android -lETC1
+           -lpng -L/usr/lib/$(DEB_HOST_MULTIARCH)/android \
+           -Ldebian/out/frameworks/native -lETC1
 
 development/$(NAME): $(SOURCES)
 	$(CXX) $^ -o $@ $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
