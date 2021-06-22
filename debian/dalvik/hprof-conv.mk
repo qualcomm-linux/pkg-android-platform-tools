4b5439c18a6df1cfe364405eaf08d7b8cc752ce1
Prateek Ganguli <prateek.ganguli@gmail.com>
2021-06-22 20:29:03 +0530

    Static link libs in `dalvik`

--- debian/dalvik/hprof-conv.mk
+++ debian/dalvik/hprof-conv.mk
@@ -1,7 +1,7 @@
 NAME = hprof-conv
-SOURCES = HprofConv.c
-SOURCES := $(foreach source, $(SOURCES), dalvik/tools/hprof-conv/$(source))
+
+SOURCES = dalvik/tools/hprof-conv/HprofConv.c
 
 debian/out/dalvik/tools/$(NAME): $(SOURCES)
 	mkdir --parents debian/out/dalvik/tools
-	$(CC) $^ -o debian/out/dalvik/tools/$(NAME) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS)
+	$(CC) -o $@ $^ $(CFLAGS) $(CPPFLAGS) $(LDFLAGS)
