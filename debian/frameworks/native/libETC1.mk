978039abe1e85df3fe12570c54764599d396726a
Prateek Ganguli <prateek.ganguli@gmail.com>
2021-06-22 20:29:53 +0530

    Static link libs in `frameworks/native`

--- debian/frameworks/native/libETC1.mk
+++ debian/frameworks/native/libETC1.mk
@@ -1,10 +1,13 @@
 NAME = libETC1
-SOURCES_libETC1 = etc1.cpp
-OBJECTS = $(SOURCES_libETC1:.cpp=.o)
-SOURCES := $(foreach source, $(SOURCES_libETC1), frameworks/native/opengl/libs/ETC1/$(source))
+
+SOURCES = frameworks/native/opengl/libs/ETC1/etc1.cpp
+OBJECTS = $(SOURCES:.cpp=.o)
+
 CPPFLAGS += -Iframeworks/native/opengl/include
 
-debian/out/frameworks/native/$(NAME).a: $(SOURCES)
+debian/out/frameworks/native/$(NAME).a: $(OBJECTS)
 	mkdir --parents debian/out/frameworks/native
-	$(CXX) -c $^ $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
-	ar -rcs $@ $(OBJECTS)
+	ar -rcs $@ $^
+
+$(OBJECTS): %.o: %.cpp
+	$(CXX) -c -o $@ $< $(CXXFLAGS) $(CPPFLAGS)
